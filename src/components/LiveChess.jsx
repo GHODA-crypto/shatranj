25655d1949cf87a27c90adbb9616d87450e77ac9
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-12-11 01:43:19 +0530

    Code split and useBoardWidth Hooks Added

--- src/components/LiveChess.jsx
+++ src/components/LiveChess.jsx
@@ -1,41 +1,25 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import {
 	useMoralisQuery,
 	useMoralisCloudFunction,
 	useMoralis,
 } from "react-moralis";
-import { GameBoard } from "./Chessboard";
 import { useWindowSize } from "../hooks/useWindowSize";
-import { Layout, Tabs, Drawer } from "antd";
-import {
-	FireOutlined,
-	InfoCircleOutlined,
-	FireFilled,
-} from "@ant-design/icons";
+import useBoardWidth from "../hooks/useBoardWidth";
 
-import { ReactComponent as Send } from "../assets/send.svg";
-import { ReactComponent as WhiteKing } from "../assets/chess_svgs/white_king.svg";
-import { ReactComponent as WhiteKnight } from "../assets/chess_svgs/white_knight.svg";
-import { ReactComponent as WhiteQueen } from "../assets/chess_svgs/white_queen.svg";
-import { ReactComponent as WhiteBishop } from "../assets/chess_svgs/white_bishop.svg";
-import { ReactComponent as WhiteRook } from "../assets/chess_svgs/white_rook.svg";
-import { ReactComponent as WhitePawn } from "../assets/chess_svgs/white_pawn.svg";
-import { ReactComponent as BlackKing } from "../assets/chess_svgs/black_king.svg";
-import { ReactComponent as BlackKnight } from "../assets/chess_svgs/black_knight.svg";
-import { ReactComponent as BlackQueen } from "../assets/chess_svgs/black_queen.svg";
-import { ReactComponent as BlackBishop } from "../assets/chess_svgs/black_bishop.svg";
-import { ReactComponent as BlackRook } from "../assets/chess_svgs/black_rook.svg";
-import { ReactComponent as BlackPawn } from "../assets/chess_svgs/black_pawn.svg";
+import TabView from "./views/TabView";
+import MobileView from "./views/MobileView";
+import DesktopView from "./views/DesktopView";
 
 import "../styles/game.scss";
+
+import { GameBoard } from "./Chessboard";
 
 const LiveChess = ({ pairingParams, isPairing, setIsPairing }) => {
 	const [gameId, setGameId] = useState();
 	const [playerSide, setPlayerSide] = useState("white");
 
-	const winSize = useWindowSize();
 	const [isMobileDrawerVisible, setIsMobileDrawerVisible] = useState(false);
-	const [isPlayerWhite, setIsPlayerWhite] = useState(false);
 
 	const { user, isInitialized } = useMoralis();
 
@@ -85,6 +69,15 @@ const LiveChess = ({ pairingParams, isPairing, setIsPairing }) => {
 		if (challenge) setGameId(challenge?.get("gameId"));
 	}, [challenge]);
 
+	const [isPlayerWhite, setIsPlayerWhite] = useMemo(() => {
+		return liveGameData
+			? liveGameData.get("sides")[user.get("ethAddress")] === "w"
+			: "w";
+	}, [liveGameData, user]);
+
+	const winSize = useWindowSize();
+	const boardWidth = useBoardWidth(winSize);
+
 	if (winSize.width < 768)
 		return (
 			<MobileView
@@ -92,14 +85,26 @@ const LiveChess = ({ pairingParams, isPairing, setIsPairing }) => {
 				setIsMobileDrawerVisible={setIsMobileDrawerVisible}
 				isPlayerWhite={isPlayerWhite}
 				setIsPlayerWhite={setIsPlayerWhite}
-			/>
+				winSize={winSize}>
+				<GameBoard
+					user={user}
+					isPlayerWhite={isPlayerWhite}
+					boardWidth={boardWidth}
+				/>
+			</MobileView>
 		);
 	else if (winSize.width >= 768 && winSize.width < 1024)
 		return (
 			<TabView
 				isPlayerWhite={isPlayerWhite}
 				setIsPlayerWhite={setIsPlayerWhite}
-			/>
+				winSize={winSize}>
+				<GameBoard
+					user={user}
+					isPlayerWhite={isPlayerWhite}
+					boardWidth={boardWidth}
+				/>
+			</TabView>
 		);
 	else
 		return (
@@ -107,378 +112,14 @@ const LiveChess = ({ pairingParams, isPairing, setIsPairing }) => {
 				joinLiveChess={joinLiveChess}
 				isPlayerWhite={isPlayerWhite}
 				setIsPlayerWhite={setIsPlayerWhite}
-			/>
+				winSize={winSize}>
+				<GameBoard
+					user={user}
+					isPlayerWhite={isPlayerWhite}
+					boardWidth={boardWidth}
+				/>
+			</DesktopView>
 		);
-};
-
-const MobileView = ({
-	isMobileDrawerVisible,
-	setIsMobileDrawerVisible,
-	isPlayerWhite,
-}) => {
-	const { user } = useMoralis();
-	const winSize = useWindowSize();
-	const { Content } = Layout;
-	const { TabPane } = Tabs;
-
-	const styles = {
-		Drawer: {
-			margin: "0",
-			padding: "1.5rem",
-			width: "100%",
-			height: "100%",
-			backgroundColor: "#041836",
-			boxShadow: "0px 0px 12px 2px rgb(88,197,99, 0.4)",
-		},
-		Button: {
-			position: "absolute",
-			top: "1rem",
-			right: "1rem",
-			width: "3rem",
-			height: "3rem",
-			borderRadius: "50%",
-			border: "none",
-			backgroundColor: "#041836",
-			color: "#58c563",
-			boxShadow: "0px 0px 12px 2px rgb(88,197,99, 0.4)",
-			cursor: "pointer",
-		},
-	};
-
-	return (
-		<Layout className="game-desktop" style={{ position: "relative" }}>
-			<button
-				className="drawer-btn"
-				onClick={() => {
-					setIsMobileDrawerVisible(!isMobileDrawerVisible);
-					console.log(isMobileDrawerVisible);
-				}}
-				style={styles.Button}>
-				<FireFilled style={{ margin: "auto", fontSize: "1.5rem" }} />
-			</button>
-			<Content className="chessboard">
-				<div className="players op">
-					<div className="player-info">
-						<div className="username">0x1234123412321432</div>
-						<div className="ilo">(1456)</div>
-					</div>
-					<div className="fallen-peice fallen-peice-op">
-						<WhiteRook size={15} />
-						<WhiteKnight size={15} />
-						<WhiteBishop size={15} />
-					</div>
-				</div>
-
-				<GameBoard
-					user={user}
-					boardWidth={Math.min(winSize.width * 0.6, winSize.height * 0.75)}
-					isPlayerWhite={isPlayerWhite}
-				/>
-
-				<div className="players self">
-					<div className="player-info">
-						<div className="username">
-							{user?.attributes?.ethAddress.slice(0, 8)}...
-							{user?.attributes?.ethAddress.slice(-9, -1)}
-						</div>
-						<div className="ilo">(1456)</div>
-					</div>
-					<div className="fallen-peice fallen-peice-self">
-						<BlackPawn size={15} />
-						<BlackQueen size={15} />
-						<BlackKing size={15} />
-					</div>
-				</div>
-			</Content>
-
-			<Drawer
-				className="game-meta"
-				placement="right"
-				visible={isMobileDrawerVisible}
-				onClose={() => setIsMobileDrawerVisible(false)}
-				drawerStyle={styles.Drawer}
-				width={Math.max(winSize.width * 0.3, 400)}
-				zIndex={1000}>
-				<Tabs
-					style={{
-						width: "100%",
-						display: "flex",
-						justifyContent: "space-around",
-						color: "white",
-					}}
-					defaultActiveKey="2"
-					tabBarGutter={75}
-					centered={true}>
-					<TabPane
-						tab={
-							<span>
-								<FireOutlined />
-								Chat Room
-							</span>
-						}
-						key="1"
-						className="chat-room">
-						<div className="prize-pool">
-							<span className="label">Prize Pool</span>
-							<div className="prize">
-								<span className="amount">15</span>
-								<span className="ghd">GHD</span>
-							</div>
-						</div>
-
-						<div className="chat">
-							<div className="chat-text"></div>
-							<div className="chat-input">
-								<input type="text" />
-								<button>
-									<Send />
-								</button>
-							</div>
-						</div>
-					</TabPane>
-					<TabPane
-						tab={
-							<span>
-								<InfoCircleOutlined />
-								Game Info
-							</span>
-						}
-						key="2"
-						className="game-info">
-						<div className="pgn"></div>
-						<div className="btns">
-							<button>Play Again</button>
-							<button>Button2</button>
-							<button>Button3</button>
-							<button className="danger">Button4</button>
-						</div>
-					</TabPane>
-				</Tabs>
-			</Drawer>
-		</Layout>
-	);
-};
-
-const TabView = ({ isPlayerWhite }) => {
-	const { user } = useMoralis();
-	const winSize = useWindowSize();
-	const { TabPane } = Tabs;
-	const { Content, Sider } = Layout;
-
-	const styles = {
-		Sider: {
-			margin: "0",
-			padding: "1.5rem",
-			borderRadius: "1rem",
-			width: "100%",
-			height: "100%",
-			zIndex: "1",
-		},
-	};
-
-	return (
-		<Layout className="game-desktop">
-			<Content className="chessboard">
-				<div className="players op">
-					<div className="player-info">
-						<div className="username">0x1234123412321432</div>
-						<div className="ilo">(1456)</div>
-					</div>
-					<div className="fallen-peice fallen-peice-op">
-						<WhiteRook size={15} />
-						<WhiteKnight size={15} />
-						<WhiteBishop size={15} />
-					</div>
-				</div>
-
-				<GameBoard
-					user={user}
-					boardWidth={Math.min(winSize.width * 0.6, winSize.height * 0.75)}
-					isPlayerWhite={isPlayerWhite}
-				/>
-
-				<div className="players self">
-					<div className="player-info">
-						<div className="username">
-							{user?.attributes?.ethAddress.slice(0, 8)}...
-							{user?.attributes?.ethAddress.slice(-9, -1)}
-						</div>
-						<div className="ilo">(1456)</div>
-					</div>
-					<div className="fallen-peice fallen-peice-self">
-						<BlackPawn size={15} />
-						<BlackQueen size={15} />
-						<BlackKing size={15} />
-					</div>
-				</div>
-			</Content>
-
-			<Sider
-				className="game-meta"
-				style={styles.Sider}
-				width={winSize.width * 0.3}>
-				<Tabs
-					style={{
-						width: "100%",
-						display: "flex",
-						justifyContent: "space-around",
-						color: "white",
-					}}
-					defaultActiveKey="2"
-					tabBarGutter={75}
-					centered={true}>
-					<TabPane
-						tab={
-							<span>
-								<FireOutlined />
-								Chat Room
-							</span>
-						}
-						key="1"
-						className="chat-room">
-						<div className="prize-pool">
-							<span className="label">Prize Pool</span>
-							<div className="prize">
-								<span className="amount">15</span>
-								<span className="ghd">GHD</span>
-							</div>
-						</div>
-
-						<div className="chat">
-							<div className="chat-text"></div>
-							<div className="chat-input">
-								<input type="text" />
-								<button>
-									<Send />
-								</button>
-							</div>
-						</div>
-					</TabPane>
-					<TabPane
-						tab={
-							<span>
-								<InfoCircleOutlined />
-								Game Info
-							</span>
-						}
-						key="2"
-						className="game-info">
-						<div className="pgn"></div>
-						<div className="btns">
-							<button>Play Again</button>
-							<button>Button2</button>
-							<button>Button3</button>
-							<button className="danger">Button4</button>
-						</div>
-					</TabPane>
-				</Tabs>
-			</Sider>
-		</Layout>
-	);
-};
-
-const DesktopView = ({ isPlayerWhite, joinLiveChess }) => {
-	const winSize = useWindowSize();
-	const { user } = useMoralis();
-	const styles = {
-		Sider: {
-			margin: "0",
-			padding: "1.5rem",
-			borderRadius: "1rem",
-			width: "100%",
-			zIndex: "1",
-		},
-	};
-	const { Sider, Content } = Layout;
-
-	return (
-		<Layout className="game-desktop">
-			<Sider
-				className="chat-room"
-				style={styles.Sider}
-				collapsible={true}
-				collapsedWidth={0}
-				trigger={<FireOutlined size={40} />}
-				zeroWidthTriggerStyle={{
-					backgroundColor: "rgb(255, 64, 64)",
-					borderRadius: "50%",
-					width: "3rem",
-					height: "3rem",
-					top: "5%",
-					display: "flex",
-					justifyContent: "center",
-					alignItems: "center",
-					right: "-1.5rem",
-				}}
-				width={winSize.width * 0.25}>
-				<div className="prize-pool">
-					<span className="label">Prize Pool</span>
-					<div className="prize">
-						<span className="amount">15</span>
-						<span className="ghd">GHD</span>
-					</div>
-				</div>
-
-				<div className="chat">
-					<div className="chat-text"></div>
-					<div className="chat-input">
-						<input type="text" />
-						<button>
-							<Send />
-						</button>
-					</div>
-				</div>
-			</Sider>
-
-			<Content className="chessboard">
-				<div className="players op">
-					<div className="player-info">
-						<div className="username">0x1234123412321432</div>
-						<div className="ilo">(1456)</div>
-					</div>
-					<div className="fallen-peice fallen-peice-op">
-						<WhiteRook size={15} />
-						<WhiteKnight size={15} />
-						<WhiteBishop size={15} />
-					</div>
-				</div>
-
-				<GameBoard
-					user={user}
-					boardWidth={Math.min(winSize.width * 0.5, winSize.height * 0.7)}
-					isPlayerWhite={isPlayerWhite}
-				/>
-
-				<div className="players self">
-					<div className="player-info">
-						<div className="username">
-							{user?.attributes?.ethAddress.slice(0, 8)}...
-							{user?.attributes?.ethAddress.slice(-9, -1)}
-						</div>
-						<div className="ilo">(1456)</div>
-					</div>
-					<div className="fallen-peice fallen-peice-self">
-						<BlackPawn size={15} />
-						<BlackQueen size={15} />
-						<BlackKing size={15} />
-					</div>
-				</div>
-			</Content>
-
-			<Sider
-				className="game-info"
-				style={styles.Sider}
-				width={winSize.width * 0.25}>
-				<div className="pgn"></div>
-				<div className="btns">
-					<button onClick={() => joinLiveChess()}>New Game</button>
-					<button>Something</button>
-					<button>Button3</button>
-					<button className="danger">Button4</button>
-				</div>
-			</Sider>
-		</Layout>
-	);
 };
 
 export default LiveChess;
