aa4af4a9cfb35e8792f1c98381d2f6d7cdaaf047
Pushkar Borkar <pushkarborkar1809@gmail.com>
2021-12-10 16:12:09 +0530

    Gamer layout updated to antd

--- src/components/LiveChess.jsx
+++ src/components/LiveChess.jsx
@@ -7,6 +7,12 @@ import {
 } from "react-moralis";
 import { GameBoard } from "./Chessboard";
 import { useWindowSize } from "../hooks/useWindowSize";
+import { Layout, Tabs, Drawer } from "antd";
+import {
+	FireOutlined,
+	InfoCircleOutlined,
+	FireFilled,
+} from "@ant-design/icons";
 
 import { ReactComponent as Send } from "../assets/send.svg";
 import { ReactComponent as WhiteKing } from "../assets/chess_svgs/white_king.svg";
@@ -28,7 +34,16 @@ const LiveChess = ({ pairingParams, isPairing }) => {
 	const winSize = useWindowSize();
 	const [currentTabletPage, setCurrentTabletPage] = useState(1);
 	const [currentMobilePage, setCurrentMobilePage] = useState(2);
-	const { isWeb3Enabled, Moralis, web3, user } = useMoralis();
+	const [isMobileDrawerVisible, setIsMobileDrawerVisible] = useState(false);
+
+	const {
+		isWeb3Enabled,
+		Moralis,
+		isWeb3EnableLoading,
+		enableWeb3,
+		web3,
+		user,
+	} = useMoralis();
 	// Proxy address, Privatekey, Signature
 	const proxyAccount = useMemo(() => {
 		if (isWeb3Enabled) {
@@ -43,6 +58,19 @@ const LiveChess = ({ pairingParams, isPairing }) => {
 				localStorage.setItem("proxyPrivKey", proxyAccount.privateKey);
 			}
 
+			const {
+				data: [gameData],
+				// error: gameError,
+				isLoading: isGameLoading,
+			} = useMoralisQuery(
+				"Game",
+				(query) => query.equalTo("objectId", gameId).limit(1),
+				[gameId],
+				{
+					autoFetch: false,
+					live: true,
+				}
+			);
 			return {
 				address: proxyAccount.address,
 				privateKey: proxyAccount.privateKey,
@@ -76,12 +104,13 @@ const LiveChess = ({ pairingParams, isPairing }) => {
 	);
 
 	const {
-		data: [gameData],
+		fetch: fetchGame,
+		data: gameData,
 		// error: gameError,
 		isLoading: isGameLoading,
 	} = useMoralisQuery(
 		"Game",
-		(query) => query.equalTo("objectId", gameId).limit(1),
+		(query) => query.find(gameId || "hPUavu3nkjFf5QAcdJp10cEh").limit(1),
 		[gameId],
 		{
 			autoFetch: false,
@@ -90,176 +119,83 @@ const LiveChess = ({ pairingParams, isPairing }) => {
 	);
 
 	useEffect(() => {
-		getChallenge();
+		initLiveChess();
 	}, []);
-
+	useEffect(() => {
+		gameId && fetchGame();
+	}, [gameId]);
 	useEffect(() => {
 		console.log("gameData", gameData);
 	}, [gameData]);
-	useEffect(() => {
-		challenge && initLiveChess();
-		console.log("challenge", challenge);
-	}, [challenge]);
 
 	const initLiveChess = async () => {
-		const userPlayerNumber =
-			challenge.player1 === user.get("ethAddress") ? 1 : 2;
-		const challengeIdHex = idToHex(challenge.objectId);
-
-		// Accept the challenge and sign if proxy is not set
-		if (!challenge[`p${userPlayerNumber}proxy`]) {
+		await getChallenge();
+		if (challenge) {
+			const challengeIdHex = idToHex(challenge.objectId);
 			const signature = await signGameAndProxy(challengeIdHex);
+
 			const gameId = await Moralis.Cloud.run("acceptChallenge", {
 				challengeIdHex: challengeIdHex,
 				proxyAddress: proxyAccount?.address,
 				signature,
 			});
 			setGameId(gameId);
-		} else {
-			// Set Game Id to Challenge Id
-			setGameId(challenge.gameId);
 		}
 	};
 
-	if (winSize.width > 1400) {
-		return <DesktopView user={user} initLiveChess={initLiveChess} />;
-	} else if (winSize.width > 768 && winSize.width < 1400) {
-		return (
-			<TabView
-				user={user}
-				currentTabletPage={currentTabletPage}
-				setCurrentTabletPage={setCurrentTabletPage}
-				initLiveChess={initLiveChess}
-			/>
-		);
-	} else {
-		return (
-			<MobileView
-				user={user}
-				currentMobilePage={currentMobilePage}
-				setCurrentMobilePage={setCurrentMobilePage}
-				initLiveChess={initLiveChess}
-			/>
-		);
-	}
-};
-
-const MobileView = ({
-	user,
-	currentMobilePage,
-	setCurrentMobilePage,
-	initLiveChess,
-}) => {
-	const winSize = useWindowSize();
 	return (
-		<div className="game-mobile">
-			<main className="content">
-				{currentMobilePage === 1 && (
-					<section className="prize-chat">
-						<div className="prize-pool">
-							<span className="label">Prize Pool</span>
-							<div className="prize">
-								<span className="amount">15</span>
-								<span className="ghd">GHD</span>
-							</div>
-						</div>
-						<div className="chat">
-							<div className="chat-text"></div>
-							<div className="chat-input">
-								<input type="text" />
-								<button>
-									<Send />
-								</button>
-							</div>
-						</div>
-					</section>
-				)}
-
-				{currentMobilePage === 2 && (
-					<section className="chessboard">
-						<div className="players op">
-							<div className="player-info">
-								<div className="username">0x1234123412321432</div>
-								<div className="ilo">(1456)</div>
-							</div>
-							<div className="fallen-peice fallen-peice-op">
-								<WhiteRook size={15} />
-								<WhiteKnight size={15} />
-								<WhiteBishop size={15} />
-							</div>
-						</div>
-
-						<GameBoard
-							user={user}
-							boardWidth={Math.min(winSize.width * 0.45, winSize.height * 0.7)}
-						/>
-
-						<div className="players self">
-							<div className="player-info">
-								<div className="username">
-									{user?.attributes?.ethAddress.slice(0, 8)}...
-									{user?.attributes?.ethAddress.slice(-9, -1)}
-								</div>
-								<div className="ilo">(1456)</div>
-							</div>
-							<div className="fallen-peice fallen-peice-self">
-								<BlackPawn size={15} />
-								<BlackQueen size={15} />
-								<BlackKing size={15} />
-							</div>
-						</div>
-					</section>
-				)}
-
-				{currentMobilePage === 3 && (
-					<section className="game-info">
-						<div className="pgn"></div>
-						<div className="btns">
-							<button>Button1</button>
-							<button>Button2</button>
-							<button>Button3</button>
-							<button className="danger">Button4</button>
-						</div>
-					</section>
-				)}
-			</main>
-			<div className="tab-bar">
-				<div
-					id={currentMobilePage === 1 ? "active" : ""}
-					className="chat-room-tab tab"
-					onClick={() => setCurrentMobilePage(1)}>
-					Chat Room
-				</div>
-
-				<div
-					id={currentMobilePage === 2 ? "active" : ""}
-					className="game-tab tab"
-					onClick={() => setCurrentMobilePage(2)}>
-					Game
-				</div>
-
-				<div
-					id={currentMobilePage === 3 ? "active" : ""}
-					className="game-info-tab tab"
-					onClick={() => setCurrentMobilePage(3)}>
-					Game Info
-				</div>
-			</div>
-		</div>
+		<MobileView
+			isMobileDrawerVisible={isMobileDrawerVisible}
+			setIsMobileDrawerVisible={setIsMobileDrawerVisible}
+		/>
 	);
+	// 1200 - 1600
+
+	// 	<TabView
+	// 	user={user}
+	// 	currentTabletPage={currentTabletPage}
+	// 	setCurrentTabletPage={setCurrentTabletPage}
+	// />
 };
 
-const TabView = ({
-	user,
-	currentTabletPage,
-	setCurrentTabletPage,
-	initLiveChess,
-}) => {
+const MobileView = ({ isMobileDrawerVisible, setIsMobileDrawerVisible }) => {
+	const { user } = useMoralis();
 	const winSize = useWindowSize();
+	const { Content } = Layout;
+	const { TabPane } = Tabs;
+
+	const styles = {
+		Sider: {
+			margin: "0",
+			padding: "1.5rem",
+			borderRadius: "1rem",
+			width: "100%",
+			height: "100%",
+			zIndex: "1",
+		},
+		Button: {
+			position: "absolute",
+			top: "1rem",
+			right: "1rem",
+			width: "3rem",
+			height: "3rem",
+			borderRadius: "50%",
+			border: "none",
+			backgroundColor: "#041836",
+			color: "#58c563",
+			boxShadow: "0px 0px 12px 2px rgb(88,197,99, 0.4)",
+		},
+	};
 
 	return (
-		<div className="game-tablet">
-			<section className="chessboard">
+		<Layout className="game-desktop" style={{ position: "relative" }}>
+			<button
+				className="drawer-btn"
+				onClick={() => setIsMobileDrawerVisible(true)}
+				style={styles.Button}>
+				<FireFilled style={{ margin: "auto", fontSize: "1.5rem" }} />
+			</button>
+			<Content className="chessboard">
 				<div className="players op">
 					<div className="player-info">
 						<div className="username">0x1234123412321432</div>
@@ -274,7 +210,7 @@ const TabView = ({
 
 				<GameBoard
 					user={user}
-					boardWidth={Math.min(winSize.width * 0.45, winSize.height * 0.7)}
+					boardWidth={Math.min(winSize.width * 0.6, winSize.height * 0.75)}
 				/>
 
 				<div className="players self">
@@ -291,68 +227,226 @@ const TabView = ({
 						<BlackKing size={15} />
 					</div>
 				</div>
-			</section>
+			</Content>
 
-			<section className="game-meta">
-				<div className="tab-bar">
-					<div
-						id={currentTabletPage === 1 ? "active" : ""}
-						className="chat-room-tab tab"
-						onClick={() => setCurrentTabletPage(1)}>
-						Chat Room
-					</div>
-					<div
-						id={currentTabletPage === 2 ? "active" : ""}
-						className="game-info-tab tab"
-						onClick={() => setCurrentTabletPage(2)}>
-						Game Info
-					</div>
-				</div>
-				<div className="tab-content">
-					{currentTabletPage === 1 && (
-						<>
-							<div className="prize-pool">
-								<span className="label">Prize Pool</span>
-								<div className="prize">
-									<span className="amount">15</span>
-									<span className="ghd">GHD</span>
-								</div>
+			<Drawer
+				className="game-meta"
+				placement="right"
+				visible={isMobileDrawerVisible}
+				onClose={() => setIsMobileDrawerVisible(false)}
+				style={styles.Sider}
+				width={winSize.width * 0.3}>
+				<Tabs
+					style={{
+						width: "100%",
+						display: "flex",
+						justifyContent: "space-around",
+						color: "white",
+					}}
+					defaultActiveKey="2"
+					tabBarGutter={75}
+					centered={true}>
+					<TabPane
+						tab={
+							<span>
+								<FireOutlined />
+								Chat Room
+							</span>
+						}
+						key="1"
+						className="chat-room">
+						<div className="prize-pool">
+							<span className="label">Prize Pool</span>
+							<div className="prize">
+								<span className="amount">15</span>
+								<span className="ghd">GHD</span>
 							</div>
-							<div className="chat">
-								<div className="chat-text"></div>
-								<div className="chat-input">
-									<input type="text" />
-									<button>
-										<Send />
-									</button>
-								</div>
-							</div>
-						</>
-					)}
+						</div>
 
-					{currentTabletPage === 2 && (
-						<>
-							<div className="pgn"></div>
-							<div className="btns">
-								<button>Button1</button>
-								<button>Button2</button>
-								<button>Button3</button>
-								<button className="danger">Button4</button>
+						<div className="chat">
+							<div className="chat-text"></div>
+							<div className="chat-input">
+								<input type="text" />
+								<button>
+									<Send />
+								</button>
 							</div>
-						</>
-					)}
-				</div>
-			</section>
-		</div>
+						</div>
+					</TabPane>
+					<TabPane
+						tab={
+							<span>
+								<InfoCircleOutlined />
+								Game Info
+							</span>
+						}
+						key="2"
+						className="game-info">
+						<div className="pgn"></div>
+						<div className="btns">
+							<button>Play Again</button>
+							<button>Button2</button>
+							<button>Button3</button>
+							<button className="danger">Button4</button>
+						</div>
+					</TabPane>
+				</Tabs>
+			</Drawer>
+		</Layout>
 	);
 };
 
-const DesktopView = ({ user, initLiveChess }) => {
+const TabView = () => {
+	const { user } = useMoralis();
 	const winSize = useWindowSize();
+	const { TabPane } = Tabs;
+	const { Content, Sider } = Layout;
+
+	const styles = {
+		Sider: {
+			margin: "0",
+			padding: "1.5rem",
+			borderRadius: "1rem",
+			width: "100%",
+			height: "100%",
+			zIndex: "1",
+		},
+	};
 
 	return (
-		<div className="game-desktop">
-			<section className="game-btns">
+		<Layout className="game-desktop">
+			<Content className="chessboard">
+				<div className="players op">
+					<div className="player-info">
+						<div className="username">0x1234123412321432</div>
+						<div className="ilo">(1456)</div>
+					</div>
+					<div className="fallen-peice fallen-peice-op">
+						<WhiteRook size={15} />
+						<WhiteKnight size={15} />
+						<WhiteBishop size={15} />
+					</div>
+				</div>
+
+				<GameBoard
+					user={user}
+					boardWidth={Math.min(winSize.width * 0.6, winSize.height * 0.75)}
+				/>
+
+				<div className="players self">
+					<div className="player-info">
+						<div className="username">
+							{user?.attributes?.ethAddress.slice(0, 8)}...
+							{user?.attributes?.ethAddress.slice(-9, -1)}
+						</div>
+						<div className="ilo">(1456)</div>
+					</div>
+					<div className="fallen-peice fallen-peice-self">
+						<BlackPawn size={15} />
+						<BlackQueen size={15} />
+						<BlackKing size={15} />
+					</div>
+				</div>
+			</Content>
+
+			<Sider
+				className="game-meta"
+				style={styles.Sider}
+				width={winSize.width * 0.3}>
+				<Tabs
+					style={{
+						width: "100%",
+						display: "flex",
+						justifyContent: "space-around",
+						color: "white",
+					}}
+					defaultActiveKey="2"
+					tabBarGutter={75}
+					centered={true}>
+					<TabPane
+						tab={
+							<span>
+								<FireOutlined />
+								Chat Room
+							</span>
+						}
+						key="1"
+						className="chat-room">
+						<div className="prize-pool">
+							<span className="label">Prize Pool</span>
+							<div className="prize">
+								<span className="amount">15</span>
+								<span className="ghd">GHD</span>
+							</div>
+						</div>
+
+						<div className="chat">
+							<div className="chat-text"></div>
+							<div className="chat-input">
+								<input type="text" />
+								<button>
+									<Send />
+								</button>
+							</div>
+						</div>
+					</TabPane>
+					<TabPane
+						tab={
+							<span>
+								<InfoCircleOutlined />
+								Game Info
+							</span>
+						}
+						key="2"
+						className="game-info">
+						<div className="pgn"></div>
+						<div className="btns">
+							<button>Play Again</button>
+							<button>Button2</button>
+							<button>Button3</button>
+							<button className="danger">Button4</button>
+						</div>
+					</TabPane>
+				</Tabs>
+			</Sider>
+		</Layout>
+	);
+};
+
+const DesktopView = ({}) => {
+	const winSize = useWindowSize();
+	const { user } = useMoralis();
+	const styles = {
+		Sider: {
+			margin: "0",
+			padding: "1.5rem",
+			borderRadius: "1rem",
+			width: "100%",
+			zIndex: "1",
+		},
+	};
+	const { Sider, Content } = Layout;
+
+	return (
+		<Layout className="game-desktop">
+			<Sider
+				className="chat-room"
+				style={styles.Sider}
+				collapsible={true}
+				collapsedWidth={0}
+				trigger={<FireOutlined size={40} />}
+				zeroWidthTriggerStyle={{
+					backgroundColor: "rgb(255, 64, 64)",
+					borderRadius: "50%",
+					width: "3rem",
+					height: "3rem",
+					top: "5%",
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					right: "-1.5rem",
+				}}
+				width={winSize.width * 0.25}>
 				<div className="prize-pool">
 					<span className="label">Prize Pool</span>
 					<div className="prize">
@@ -370,8 +464,9 @@ const DesktopView = ({ user, initLiveChess }) => {
 						</button>
 					</div>
 				</div>
-			</section>
-			<section className="chessboard">
+			</Sider>
+
+			<Content className="chessboard">
 				<div className="players op">
 					<div className="player-info">
 						<div className="username">0x1234123412321432</div>
@@ -386,7 +481,7 @@ const DesktopView = ({ user, initLiveChess }) => {
 
 				<GameBoard
 					user={user}
-					boardWidth={Math.min(winSize.width * 0.45, winSize.height * 0.7)}
+					boardWidth={Math.min(winSize.width * 0.5, winSize.height * 0.7)}
 				/>
 
 				<div className="players self">
@@ -403,17 +498,21 @@ const DesktopView = ({ user, initLiveChess }) => {
 						<BlackKing size={15} />
 					</div>
 				</div>
-			</section>
-			<section className="game-info">
+			</Content>
+
+			<Sider
+				className="game-info"
+				style={styles.Sider}
+				width={winSize.width * 0.25}>
 				<div className="pgn"></div>
 				<div className="btns">
-					<button onClick={() => initLiveChess()}>Play Again</button>
+					<button>Play Again</button>
 					<button>Button2</button>
 					<button>Button3</button>
 					<button className="danger">Button4</button>
 				</div>
-			</section>
-		</div>
+			</Sider>
+		</Layout>
 	);
 };
 
