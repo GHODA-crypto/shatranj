8dfdecf943f7246dd526547f43959505e8ae030e
Pushkar Borkar <pushkarborkar1809@gmail.com>
2021-12-11 19:14:24 +0530

    desktop view upgraded

--- src/hooks/useBoardWidth.js
+++ src/hooks/useBoardWidth.js
@@ -3,9 +3,9 @@ import { useWindowSize } from "./useWindowSize";
 const useBoardWidth = () => {
 	const winSize = useWindowSize();
 	if (winSize.width < 700)
-		return Math.min(winSize.width * 0.8, winSize.height * 0.75);
+		return Math.min(winSize.width * 0.8, winSize.height * 0.9);
 	else if (winSize.width >= 700 && winSize.width < 1024)
 		return Math.min(winSize.width * 0.6, winSize.height * 0.75);
-	else return Math.min(winSize.width * 0.45, winSize.height * 0.7);
+	else return winSize.width * 0.48;
 };
 export default useBoardWidth;
