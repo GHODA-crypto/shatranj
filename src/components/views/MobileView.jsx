6ef004a0f72f8964939cc18bdf6be237b9d66d69
Pushkar Borkar <pushkarborkar1809@gmail.com>
2021-12-12 16:56:34 +0530

    svg imports updated

--- src/components/views/MobileView.jsx
+++ src/components/views/MobileView.jsx
@@ -8,18 +8,18 @@ import {
 } from "@ant-design/icons";
 
 import { ReactComponent as Send } from "../../assets/send.svg";
-import { ReactComponent as WhiteKing } from "../../assets/chess_svgs/white_king.svg";
-import { ReactComponent as WhiteKnight } from "../../assets/chess_svgs/white_knight.svg";
-import { ReactComponent as WhiteQueen } from "../../assets/chess_svgs/white_queen.svg";
-import { ReactComponent as WhiteBishop } from "../../assets/chess_svgs/white_bishop.svg";
-import { ReactComponent as WhiteRook } from "../../assets/chess_svgs/white_rook.svg";
-import { ReactComponent as WhitePawn } from "../../assets/chess_svgs/white_pawn.svg";
-import { ReactComponent as BlackKing } from "../../assets/chess_svgs/black_king.svg";
-import { ReactComponent as BlackKnight } from "../../assets/chess_svgs/black_knight.svg";
-import { ReactComponent as BlackQueen } from "../../assets/chess_svgs/black_queen.svg";
-import { ReactComponent as BlackBishop } from "../../assets/chess_svgs/black_bishop.svg";
-import { ReactComponent as BlackRook } from "../../assets/chess_svgs/black_rook.svg";
-import { ReactComponent as BlackPawn } from "../../assets/chess_svgs/black_pawn.svg";
+import { ReactComponent as WhiteKing } from "../../assets/chess_svgs/k_w.svg";
+import { ReactComponent as WhiteKnight } from "../../assets/chess_svgs/n_w.svg";
+import { ReactComponent as WhiteQueen } from "../../assets/chess_svgs/q_w.svg";
+import { ReactComponent as WhiteBishop } from "../../assets/chess_svgs/b_w.svg";
+import { ReactComponent as WhiteRook } from "../../assets/chess_svgs/r_w.svg";
+import { ReactComponent as WhitePawn } from "../../assets/chess_svgs/p_w.svg";
+import { ReactComponent as BlackKing } from "../../assets/chess_svgs/k_b.svg";
+import { ReactComponent as BlackKnight } from "../../assets/chess_svgs/n_b.svg";
+import { ReactComponent as BlackQueen } from "../../assets/chess_svgs/q_b.svg";
+import { ReactComponent as BlackBishop } from "../../assets/chess_svgs/b_b.svg";
+import { ReactComponent as BlackRook } from "../../assets/chess_svgs/r_b.svg";
+import { ReactComponent as BlackPawn } from "../../assets/chess_svgs/p_b.svg";
 
 import { ReactComponent as Abort } from "../../assets/abort.svg";
 import { ReactComponent as Draw } from "../../assets/draw.svg";
