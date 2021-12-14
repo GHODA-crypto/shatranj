f38eb2519dd4047394baf0ed2e551006ee032a0b
Pushkar Borkar <pushkarborkar1809@gmail.com>
2021-12-14 20:32:17 +0530

    custom peices stuff

--- src/components/ChessBoards/customPieces.js
+++ src/components/ChessBoards/customPieces.js
@@ -1,21 +1,21 @@
-const pieces = [
-	"wP",
-	"wN",
-	"wB",
-	"wR",
-	"wQ",
-	"wK",
-	"bP",
-	"bN",
-	"bB",
-	"bR",
-	"bQ",
-	"bK",
-];
+const pieces = {
+	wP: "./assets/chess_pieces_png/wP.png",
+	wN: "./assets/chess_pieces_png/wN.png",
+	wB: "./assets/chess_pieces_png/wB.png",
+	wR: "./assets/chess_pieces_png/wR.png",
+	wQ: "./assets/chess_pieces_png/wQ.png",
+	wK: "./assets/chess_pieces_png/wK.png",
+	bP: "./assets/chess_pieces_png/bP.png",
+	bN: "./assets/chess_pieces_png/bN.png",
+	bB: "./assets/chess_pieces_png/bB.png",
+	bR: "./assets/chess_pieces_png/bR.png",
+	bQ: "./assets/chess_pieces_png/bQ.png",
+	bK: "./assets/chess_pieces_png/bK.png",
+};
 
 export const customPieces = () => {
 	const returnPieces = {};
-	pieces.forEach((p) => {
+	Object.keys(pieces).forEach((p) => {
 		returnPieces[p] = ({ squareWidth }) => (
 			<img
 				style={{ width: squareWidth, height: squareWidth }}
